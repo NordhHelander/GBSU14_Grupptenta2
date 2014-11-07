229f44c8692c371e4426ed3db4850e37ec8e2cd9
tantnordh <tanya . nordh @ gmail . com>
2014-11-07 12:51:45 +0100

    Hittills i WinForms

--- Grupptenta2/Grupptenta2/Program.cs
+++ Grupptenta2/Grupptenta2/Program.cs
@@ -16,7 +16,7 @@ namespace Grupptenta2
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new MainForm());
 		}
 	}
 }
