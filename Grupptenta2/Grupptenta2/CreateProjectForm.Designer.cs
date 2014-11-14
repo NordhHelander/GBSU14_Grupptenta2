d19edabf01e851f7463c4f5841d0ae3bf08097b9
KristofferHelander <kristoffer.helander@outlook.com>
2014-11-14 03:00:56 +0100

    Skapa projekt med personer som sparas till XML

---
+++ Grupptenta2/Grupptenta2/CreateProjectForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Grupptenta2
+{
+    partial class CreateProjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.lstBx_Participants = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Namn";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(58, 13);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(181, 20);
+            this.nameBox.TabIndex = 1;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(164, 226);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 23);
+            this.btn_Save.TabIndex = 2;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // lstBx_Participants
+            // 
+            this.lstBx_Participants.FormattingEnabled = true;
+            this.lstBx_Participants.Location = new System.Drawing.Point(69, 40);
+            this.lstBx_Participants.Name = "lstBx_Participants";
+            this.lstBx_Participants.Size = new System.Drawing.Size(167, 95);
+            this.lstBx_Participants.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Deltagare";
+            // 
+            // CreateProjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 261);
+            this.Controls.Add(this.lstBx_Participants);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateProjectForm";
+            this.Text = "CreateProjectForm";
+            this.Load += new System.EventHandler(this.CreateProjectForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.ListBox lstBx_Participants;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Grupptenta2/Grupptenta2/CreateProjectForm.cs
@@ -0,0 +1,47 @@
+﻿using CodeBase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestClasses;
+
+namespace Grupptenta2
+{
+    public partial class CreateProjectForm : Form
+    {
+        private static ProjectManager _projectManager;
+        private static PersonManager _personManager;
+
+        public CreateProjectForm(ProjectManager projectManager, PersonManager personManager)
+        {
+            _projectManager = projectManager;
+            _personManager = personManager;
+
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateProjectForm_Load(object sender, EventArgs e)
+        {
+            lstBx_Participants.DataSource = _personManager._persons;
+            lstBx_Participants.DisplayMember = _personManager._persons.ToString();
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            _projectManager.CreateProject(nameBox.Text, (Person)lstBx_Participants.SelectedItem);
+            int indexOfNewProject = _projectManager.GetProjects().Count - 1;
+            Project project = _projectManager.GetProjects()[indexOfNewProject];
+            this.Close();
+        }
+    }
+}
